384f4aab5f4924fcb97773c4688f35827b1096a2
Matthew-Schnapp <mschnapp11@gmail.com>
2021-01-05 16:53:20 -0800

    learned how to make colored screen, make a rectangle, and make it move across screen.

--- main.py
+++ main.py
@@ -4,21 +4,31 @@ import pygame
 
 BLACK = pygame.Color('black')
 WHITE = pygame.Color('white')
+TURQUOISE = pygame.Color(35, 200, 170);
+
 
 def tprint(string):
     print(string)
 
+
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((500, 700))
+    screen = pygame.display.set_mode((700, 700))
     clock = pygame.time.Clock()
 
-    ticks = 30
-    while(True):
-        screen.fill(WHITE)
-        pygame.display.flip()
-        clock.tick(ticks)
 
-        screen.fill(BLACK)
+
+    ticks = 30
+    i = 0
+    j = 5
+
+    while True:
+        screen.fill(WHITE)
+        rectangle = pygame.Rect(i, i, j, j)
+        pygame.draw.rect(screen, BLACK, rectangle, 4)
         pygame.display.flip()
+
+        i = i+5
+        j = (j+1)%50 + 5
+
         clock.tick(ticks)
